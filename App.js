f0c0802e61d0df1a915f3c8e49a9a0eda54d3d23
acooper <acooper@expediagroup.com>
2021-11-24 00:23:19 +0100

    Add Nav

--- App.js
+++ App.js
@@ -1,15 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const Categories = [
+  {
+    title: "Bottom Bracket" 
+  },
+  {
+    title: "Brake Levers"  
+  },
+  { 
+    title: "Brake Calipers"
+  },
+  {
+    title: "Chainsets"
+  },
+  {
+    title: "Freewheels"
+  },
+  {
+    title: "Front Deraileurs"
+  },
+  {
+    title: "Gear Levers"
+  },
+  {
+    title: "Handlebars"
+  },
+  {
+    title: "Headsets"
+  },
+  {
+    title: "Hubs"
+  },
+  {
+    title:"Pedals"
+  },
+  {
+    title: "Rear Deraileurs"
+  },
+  {
+    title: "Rims"
+  },
+  {
+    title: "Saddles"
+  },
+  {
+    title: "Seat Posts"
+  },
+  {
+    title: "Stems"
+  },
+  {
+    title: "Tyres"
+  }
+]
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailScreen} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const HomeScreen =({ navigation} ) => {
+  return ( 
+  <View style={styles}>
+    {
+      Categories.map(x => 
+        <Button title={x.title} onPress={() => navigation.navigate('Details', {name: x.title})}/>
+      )
+    }
+    </View>
+  )
+}
+
+const DetailScreen = ({ route, navigation }) => {
+  const { name } = route.params;
+  return <Text>This is the Detail Screen for {name}</Text>;
+};
 
 const styles = StyleSheet.create({
   container: {
